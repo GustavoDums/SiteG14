b24ef3147640e8ee0666a9d844c2a96a45ac0016
GustavoDums <you@example.com>
2025-01-23 09:55:03 -0300

    a

---
+++ scritp/galeriaJS.js
@@ -0,0 +1,16 @@
+
+var thumbnails = new Swiper('.slide-thumbnail', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    watchSlidesProgress: true,
+})
+
+
+var slide = new Swiper('.slide-imagens', {
+    effect: 'fade',
+    speed: 800,
+    grabCursor: true,
+    thumbs: {
+        swiper: thumbnails
+    }
+})
