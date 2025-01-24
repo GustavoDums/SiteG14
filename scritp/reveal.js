79ffa0e0dba76c7d4030baff3adc8db1a56b65d2
GustavoDums <you@example.com>
2025-01-24 11:53:12 -0300

    a

--- scritp/reveal.js
+++ scritp/reveal.js
@@ -14,15 +14,15 @@ revelar.reveal('.efeito-logo',
 revelar.reveal('.efeito-title',
 {
     duration: 2000,
-    distance: '100px',
-    delay: 200
+    distance: '300px',
+    delay: 100
 })
 
 revelar.reveal('.efeito-title-agenda',
 {
     duration: 2200,
     distance: '150px',
-    delay: 1100
+    delay: 500
 })
 
 // textos do site
@@ -31,7 +31,7 @@ revelar.reveal('.efeito-subtitle',
 {
     duration: 2200,
     distance: '200px',
-    delay: 600,
+    delay: 400,
     origin: 'right'
 })
 
@@ -39,7 +39,7 @@ revelar.reveal('.efeito-texto',
 {
     duration: 2100,
     distance: '250px',
-    delay: 1000,
+    delay: 600,
     origin: 'left'
 })
 
@@ -55,7 +55,7 @@ revelar.reveal('.efeito-subtitle-localização',
 {
     duration: 2000,
     distance: '350px',
-    delay: 1000,
+    delay: 200,
     origin: 'left'
 })
 
@@ -65,6 +65,7 @@ revelar.reveal('.efeito-iframe-maps',
 {
     duration: 2000,
     distance: '350px',
-    delay: 1000,
+    delay: 400,
     origin: 'right'
 })
+
