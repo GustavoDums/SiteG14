107b1e266542ffae1c5f4c188ebc4b68153daa33
GustavoDums <165341915+GustavoDums@users.noreply.github.com>
2025-01-30 21:36:31 -0300

    criado formulario

--- scritp/script.js
+++ scritp/script.js
@@ -26,3 +26,24 @@ function fechar() {
         navToggle.setAttribute('aria-expanded', 'false'); // Atualiza o atributo aria-expanded
     }
 }
+
+function enviarFormulario() {
+    // Pegando os valores dos campos
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var msg = document.getElementById('msg').value;
+
+    // Verificando se os campos não estão vazios
+    if (nome && email && msg) {
+        // Codificando a mensagem para que possa ser usada na URL
+        var mensagem = `*Nome:* ${nome}%0A*Email:* ${email}%0A*Mensagem:* ${msg}`;
+
+        // Criando a URL do WhatsApp
+        var whatsappURL = `https://api.whatsapp.com/send?phone=5547996073554&text=${mensagem}`;
+
+        // Redirecionando para o WhatsApp
+        window.open(whatsappURL, '_blank');
+    } else {
+        alert('Por favor, preencha todos os campos.');
+    }
+}
