2004138d5e85e2f6070b5eef96835e3f00665c2c
GustavoDums <you@example.com>
2025-02-20 10:30:12 -0300

    Arrumado padding e foto principal

--- scritp/script.js
+++ scritp/script.js
@@ -93,7 +93,25 @@ function checkDeviceType() {
     }
 }
 
+
+// JS para suavizar o efeito de rolagem
 // Chama a função para verificar o tipo de dispositivo ao carregar a página
 window.addEventListener('load', checkDeviceType);
 // Também chama a função quando o tamanho da tela for alterado (em dispositivos móveis)
 window.addEventListener('resize', checkDeviceType);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      const target = document.querySelector(this.getAttribute('href'));
+  
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop - 50, // valor para evitar sobreposição com a navbar fixa
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
